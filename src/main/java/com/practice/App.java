9ca18a97a1bfbbd18ed42f34d148a1b334c41769
yashodhar <yash9288dodabhangi@gmail.com>
2026-01-20 14:24:44 +0530

    this line is updated by yashodhar commit

--- src/main/java/com/practice/App.java
+++ src/main/java/com/practice/App.java
@@ -3,6 +3,7 @@ package com.practice;
 public class App {
     public static void main(String[] args) {
         System.out.println("Java practice ready.");
+        System.out.println("this liner has been added by yashodhar .");
     }
 
     public int add(int a, int b) {
